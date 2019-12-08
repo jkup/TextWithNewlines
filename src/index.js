4482095114ca8a6e0d71863dc3c4d6f07ca92fb8
Jon Kuperman <kuperman@adobe.com>
2019-12-07 23:33:11 -0800

    basic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-export default TextWithNewlines = props => {
-  return props.children
-    .split("\n")
-    .map((paragraph, i) => <p key={i}>{paragraph}</p>);
+import React from "react";
+
+module.exports = props => {
+  return props.children.split("\n").map((paragraph, i) => {
+    return paragraph.trim().length > 0 ? <p key={i}>{paragraph}</p> : null;
+  });
 };
---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+const TextWithNewlines = require("../src/index");
+
+test("Renders multiple paragraphs", () => {
+  const testText = `First Paragraph
+    
+    Second Paragraph
+    
+    Third Paragraph`;
+  const { container } = render(<TextWithNewlines>{testText}</TextWithNewlines>);
+  expect(container.querySelectorAll("p").length).toBe(3);
+});
